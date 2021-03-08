6cffd2a9f9647877779d1dcb5f9695fcf4c56b22
JMcB <joel.mcbride1@live.com>
2021-03-08 20:03:51 +0000

    Minor change to building

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 py -m pip install -U pyinstaller
 pyinstaller -y --name pog --add-data config.example.json;. main.py
-tar -c -f "zips\pog-exe.zip" "dist\pog"
+tar -c -f "zips\pog-exe.zip" -C "dist" "pog"
