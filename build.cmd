4d5233c55628ab6c45b258b3f264446e79f48b7a
JMcB <joel.mcbride1@live.com>
2021-03-08 12:51:05 +0000

    Small update to build script and add two new templates

--- build.cmd
+++ build.cmd
@@ -1,5 +1,2 @@
-del /s /q build
-del /s /q dist
-
 py -m pip install -U pyinstaller
-pyinstaller --name pog --add-data config.example.json;. main.py
+pyinstaller -y --name pog --add-data config.example.json;. main.py
