99e9911d3473a7c532a6e6d50b66114e2cd0d8f1
JMcB <joel.mcbride1@live.com>
2021-03-09 15:47:44 +0000

    Pog

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 py -m pip install -U pyinstaller
-pyinstaller -y --name pog --add-data config.example.json;. main.py
+pyinstaller -y --name pog --icon PogChamp.ico --add-data config.example.json;. main.py
 tar -c -f "zips\pog-exe.zip" -C "dist" "pog"
